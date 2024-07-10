669799d834f87a19be0f35a94958be9934018240
CHIKELUJANEUCHENNA <luckyjane60@gmail.com>
2024-07-10 18:00:17 +0100

    updated

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,17 +1,57 @@
-import React from 'react'
-import Image from 'next/image'
-import { topSellingProducts, pantProducts, vintageProducts } from './store'
-import { useRouter } from 'next/router';
-import styled from 'styled-components';
-import Icons from '@/components/Icons';
+import React from "react";
+import Image from "next/image";
+import { topSellingProducts, pantProducts, vintageProducts } from "./store";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import Icons from "@/components/Icons";
 
 const StyledProdductDetail = styled.div`
-display: flex;
-gap: 20px;
-
-`
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  padding: 30px;
+  p {
+    font-family: "Poppins";
+    font-weight: 400;
+    font-size: 24px;
+    color: #183864;
+  }
+  .price {
+    font-weight: 600;
+    font-size: 20px;
+  }
+  .reviews {
+    font-size: 18px;
+    color: #883992;
+  }
+  .size-btn {
+    width: 74px;
+    height: 56px;
+    border: 1px solid #b5b3b3;
+    border-radius: 4px;
+    margin: 5px;
+  }
+  .add {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    gap: 10px;
+    background: #712f79;
+    border-radius: 8px;
+    font-weight: 500;
+    font-size: 24px;
+    color: #fcfcfc;
+    border: none;
+    width: 578px;
+    height: 70px;
+    margin-top: 20px;
+  }
+`;
+const size = ["XXS", "XS", "S", "M", "L", "XL", "XXL"];
 function ProductDetails() {
-  const products = [...topSellingProducts, ...vintageProducts, ...pantProducts] 
+  const products = [...topSellingProducts, ...vintageProducts, ...pantProducts];
   const router = useRouter();
   const { id } = router.query;
   const product = products.find((p) => p.id.toString() === id);
@@ -22,23 +62,30 @@ function ProductDetails() {
 
   return (
     <StyledProdductDetail>
-        <Image src={product.image} alt='img' width={685} height={705} />
+      <Image src={product.image} alt="img" width={685} height={705} />
+      <div>
+        <p>{product.name}</p>
+        <p className="price">{product.price}</p>
         <div>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-            <div>
-              <p>3.9</p>
-              <p>See all 260 reviews</p>
-            </div>
-            <p>Color</p>
-            <p>Size guide</p>
-            <div>
-              <button>XXS</button>
-            </div>
-            <button><Icons type='cart' />Add To Cart</button>
+          <p>3.9</p>
+          <p className="reviews">See all 260 reviews</p>
         </div>
+        <p>Color</p>
+        <p className="reviews" style={{ textAlign: "right" }}>
+          Size guide
+        </p>
+        <div>
+          {size.map((item, i) => (
+            <button key={i} className="size-btn">{item}</button>
+          ))}
+        </div>
+        <button className="add">
+          <Icons type="add-cart" />
+          Add To Cart
+        </button>
+      </div>
     </StyledProdductDetail>
-  )
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -4,14 +4,16 @@ import styled from "styled-components";
 import Image from "next/image";
 
 const Container = styled.div`
+  overflow: hidden;
   display: flex;
-  width: 100%;
+  padding: 30px;
+  gap: 30px;
   @media (max-width: 480px) {
     flex-direction: column;
     > :first-child {
-        order: 2;
-        width: 100%;
-      }
+      order: 2;
+      width: 100%;
+    }
   }
 `;
 const ProductDetails = styled.div`
@@ -22,15 +24,49 @@ const ProductDetails = styled.div`
   color: #fff;
   p {
     font-family: "Poppins";
-  font-weight: 600;
-  font-size: 20px;
-  color: #fcfcfc;
+    font-weight: 600;
+    font-size: 20px;
+    color: #fcfcfc;
   }
 `;
 const ContactInfo = styled.div`
   width: 100%;
-
-`
+  p {
+    font-family: "Poppins";
+    font-weight: 600;
+    font-size: 24px;
+    color: #012436;
+  }
+  label {
+    font-family: "Poppins";
+    font-weight: 400;
+    font-size: 24px;
+    color: #183864;
+  }
+  input {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+    gap: 10px;
+    width: 100%;
+    height: 60px;
+    border: 1px solid #b5b3b3;
+    border-radius: 8px;
+  }
+  .shipping {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .city {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+`;
 const PayButton = styled.button`
   display: flex;
   flex-direction: row;
@@ -40,6 +76,13 @@ const PayButton = styled.button`
   background: #5e2764;
   border-radius: 8px;
   color: #fff;
+  width: 249px;
+  height: 60px;
+  font-family: "Poppins";
+  font-weight: 600;
+  font-size: 20px;
+  color: #fafafa;
+  border: none;
 `;
 const DetailsSection = styled.div`
   display: flex;
@@ -60,31 +103,55 @@ function Checkout() {
         <p>Payment details</p>
         <label>Card number</label>
         <input type="text" />
-        <div>
-          <label>Expiration date (MM/YY)</label>
-          <input type="date" />
-          <label>Cvc</label>
-          <input type="text" />
+        <div className="city">
+          <div>
+            <label>Expiration date (MM/YY)</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Cvc</label>
+            <input type="text" />
+          </div>
         </div>
         <p>Shipping address</p>
         <label>Address</label>
         <input type="text" />
-        <div>
-          <label>City</label>
-          <input type="text" />
-          <label>State/Province</label>
-          <input type="text" />
-          <label>Postal code</label>
-          <input type="text" />
+        <div className="city">
+          <div>
+            <label>City</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>State/Province</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Postal code</label>
+            <input type="text" />
+          </div>
         </div>
         <p>Billing information</p>
         <label>Same as shipping address</label>
-        <input type="checkbox" />
-        <hr />
-        <div>
+        {/* <input type="checkbox" /> */}
+        <hr
+          style={{
+            margin: "30px 0",
+          }}
+        />
+        <div className="shipping">
           <PayButton>Pay Now</PayButton>
           <p>
-            Or Continue Shopping <Icons type="arrow" />
+            Or{" "}
+            <span
+              style={{
+                fontWeight: 600,
+                fontSize: "20px",
+                color: "#4B1F50",
+              }}
+            >
+              Continue Shopping{" "}
+            </span>
+            <Icons type="arrow" />
           </p>
         </div>
       </ContactInfo>
